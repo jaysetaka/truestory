6c520faa549413eba6939e8778d660fffffa38d1
Jabulani Setaka <jsetaka@meditech.co.za>
2018-06-30 15:05:38 +0200

    make it better

--- routes/stories.js
+++ routes/stories.js
@@ -161,7 +161,7 @@ router.post('/comment/:id', (req, res) => {
     }
 
     // Add to comments array
-    story.comments.shift(newComment);
+    story.comments.push(newComment);
 
     story.save()
       .then(story => {
